0d5456707a1646f813bfef054913e6df8a4b8be4
Stardust <stardustdev@outlook.com>
2022-06-09 09:20:30 -0500

    Wrap app with CollectionContext

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CollectionProvider } from './context/CollectionContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <CollectionProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </CollectionProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
